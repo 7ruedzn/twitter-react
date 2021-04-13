9c335de56520209e8fb61ad6b422e18b03190b43
Vinicius Filipe <iits7rue@gmail.com>
2021-04-13 18:17:21 -0300

    right side container items

---
+++ src/components/Btn.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Btn = (props) => {
+    return (
+        <div className="btn-blue">
+            <h1>{props.text}</h1>
+        </div>
+    )
+}
+
+export default Btn
